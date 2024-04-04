fc4cdfcdf8538dbd7dbe10325b477d0dca9a9a4c
Vegard Falldalen <vegfall@gmail.com>
2024-04-04 20:15:29 +0200

    Added open layers map

--- src/application/application.tsx
+++ src/application/application.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import "ol/ol.css";
+import { useGeographic } from "ol/proj";
+import { OSM } from "ol/source";
+import TileLayer from "ol/layer/Tile";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({
+    center: [11, 60],
+    zoom: 10,
+  }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
 
 export default function Application() {
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+
   return (
     <>
-      <h1>Hello World! :)</h1>
+      <main ref={mapRef}></main>
     </>
   );
 }
